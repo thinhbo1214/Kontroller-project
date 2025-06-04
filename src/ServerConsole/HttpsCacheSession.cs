2baba5bb62947da0190ec32ed74b0b2391cc504b
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-06-04 09:23:29 +0700

    Source code server
    Code để server tiếp nhận giao thức https cơ bản hoàn thành

---
+++ src/ServerConsole/HttpsCacheSession.cs
@@ -0,0 +1,98 @@
+﻿using NetCoreServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConsole
+{
+    class HttpsCacheSession : HttpsSession
+    {
+        public HttpsCacheSession(NetCoreServer.HttpsServer server) : base(server) { }
+
+        protected override void OnReceivedRequest(HttpRequest request)
+        {
+            // Show HTTP request content
+            Console.WriteLine(request);
+
+            // Process HTTP request methods
+            if (request.Method == "HEAD")
+                SendResponseAsync(Response.MakeHeadResponse());
+            else if (request.Method == "GET")
+            {
+                string key = request.Url;
+
+                // Decode the key value
+                key = Uri.UnescapeDataString(key);
+                key = key.Replace("/api/cache", "", StringComparison.InvariantCultureIgnoreCase);
+                key = key.Replace("?key=", "", StringComparison.InvariantCultureIgnoreCase);
+
+                if (string.IsNullOrEmpty(key))
+                {
+                    // Response with all cache values
+                    SendResponseAsync(Response.MakeGetResponse(CommonCache.GetInstance().GetAllCache(), "application/json; charset=UTF-8"));
+                }
+                // Get the cache value by the given key
+                else if (CommonCache.GetInstance().GetCacheValue(key, out var value))
+                {
+                    // Response with the cache value
+                    SendResponseAsync(Response.MakeGetResponse(value));
+                }
+                else
+                    SendResponseAsync(Response.MakeErrorResponse(404, "Required cache value was not found for the key: " + key));
+            }
+            else if ((request.Method == "POST") || (request.Method == "PUT"))
+            {
+                string key = request.Url;
+                string value = request.Body;
+
+                // Decode the key value
+                key = Uri.UnescapeDataString(key);
+                key = key.Replace("/api/cache", "", StringComparison.InvariantCultureIgnoreCase);
+                key = key.Replace("?key=", "", StringComparison.InvariantCultureIgnoreCase);
+
+                // Put the cache value
+                CommonCache.GetInstance().PutCacheValue(key, value);
+
+                // Response with the cache value
+                SendResponseAsync(Response.MakeOkResponse());
+            }
+            else if (request.Method == "DELETE")
+            {
+                string key = request.Url;
+
+                // Decode the key value
+                key = Uri.UnescapeDataString(key);
+                key = key.Replace("/api/cache", "", StringComparison.InvariantCultureIgnoreCase);
+                key = key.Replace("?key=", "", StringComparison.InvariantCultureIgnoreCase);
+
+                // Delete the cache value
+                if (CommonCache.GetInstance().DeleteCacheValue(key, out var value))
+                {
+                    // Response with the cache value
+                    SendResponseAsync(Response.MakeGetResponse(value));
+                }
+                else
+                    SendResponseAsync(Response.MakeErrorResponse(404, "Deleted cache value was not found for the key: " + key));
+            }
+            else if (request.Method == "OPTIONS")
+                SendResponseAsync(Response.MakeOptionsResponse());
+            else if (request.Method == "TRACE")
+                SendResponseAsync(Response.MakeTraceResponse(request));
+            else
+                SendResponseAsync(Response.MakeErrorResponse("Unsupported HTTP method: " + request.Method));
+        }
+
+        protected override void OnReceivedRequestError(HttpRequest request, string error)
+        {
+            Console.WriteLine($"Request error: {error}");
+        }
+
+        protected override void OnError(SocketError error)
+        {
+            Console.WriteLine($"HTTPS session caught an error: {error}");
+        }
+    }
+}
