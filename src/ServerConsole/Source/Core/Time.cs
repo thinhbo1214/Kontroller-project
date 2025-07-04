3cac487a7faa7380da3a8e2195c835eb64a96357
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-07-04 21:57:16 +0700

    Big Update: Core System

---
+++ src/ServerConsole/Source/Core/Time.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+namespace ServerConsole.Source.Core
+{
+    public static class Time
+    {
+        private static readonly Stopwatch stopwatch = Stopwatch.StartNew();
+        public static float time => (float)stopwatch.Elapsed.TotalSeconds;
+    }
+}
