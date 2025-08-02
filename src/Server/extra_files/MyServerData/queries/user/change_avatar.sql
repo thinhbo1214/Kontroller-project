9ceb54a393f9a0a971cb3b7a21803f27272229da
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-03 00:52:15 +0700

    Comment code test to integrate

--- src/Server/extra_files/MyServerData/queries/user/change_avatar.sql
+++ src/Server/extra_files/MyServerData/queries/user/change_avatar.sql
@@ -1,5 +1,5 @@
-DECLARE @UserId UNIQUEIDENTIFIER = 'B570F1D1-B092-4A16-BD0F-F73837A5F3DF';
-DECLARE @Avatar varchar(100) = 'https://drive.google.com/file/d/1vqINurv3bx5mZOfC4zsQyHoIwM0SC_nj/view?usp=drive_link'
+-- DECLARE @UserId UNIQUEIDENTIFIER = 'B570F1D1-B092-4A16-BD0F-F73837A5F3DF';
+-- DECLARE @Avatar varchar(100) = 'https://drive.google.com/file/d/1vqINurv3bx5mZOfC4zsQyHoIwM0SC_nj/view?usp=drive_link'
 
 BEGIN TRY
     BEGIN TRANSACTION;
