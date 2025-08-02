5c252901b4b7f9e4ffc23c149536b021ce3539b7
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-02 20:33:04 +0700

    Fix error

--- src/Server/extra_files/www/FE_NOREACT/js/event.js
+++
@@ -1,7 +0,0 @@
-import { Handle } from './handle.js';
-
-document.getElementById('button-auth').addEventListener('click', () => {
-    const username = document.getElementById('loginUsername').value;
-    const password = document.getElementById('loginPassword').value;
-    Handle.Login(username, password);
-});
