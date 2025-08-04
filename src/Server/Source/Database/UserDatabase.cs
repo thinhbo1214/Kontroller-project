c4ea70cac9716a49aa91bc134afb25341267d675
Lucifer <166632710+thuangf45@users.noreply.github.com>
2025-08-04 10:41:16 +0700

    Summary for UserDatabase

--- src/Server/Source/Database/UserDatabase.cs
+++ src/Server/Source/Database/UserDatabase.cs
@@ -7,15 +7,37 @@ using Server.Source.Extra;
 
 namespace Server.Source.Database
 {
+    /// <summary>
+    /// Lớp quản lý dữ liệu bảng <c>user</c> trong cơ sở dữ liệu.
+    /// Cung cấp các chức năng cập nhật email và avatar của người dùng.
+    /// </summary>
     public class UserDatabase : BaseDatabase<User>
     {
+        /// <summary>
+        /// Singleton instance của UserDatabase.
+        /// </summary>
         private static UserDatabase _instance;
+
+        /// <summary>
+        /// Constructor riêng để ngăn tạo instance từ bên ngoài (singleton).
+        /// </summary>
         private UserDatabase() { }
 
+        /// <summary>
+        /// Truy cập instance duy nhất của UserDatabase.
+        /// </summary>
         public static UserDatabase Instance => _instance ??= new UserDatabase();
 
+        /// <summary>
+        /// Tên bảng trong cơ sở dữ liệu là "user".
+        /// </summary>
         protected override string TableName => "user";
 
+        /// <summary>
+        /// Thay đổi email người dùng.
+        /// </summary>
+        /// <param name="data">Thông tin cần thiết để thay đổi email (kiểu ParamsChangeEmail).</param>
+        /// <returns>1 nếu thành công, 0 nếu thất bại.</returns>
         public virtual int ChangeEmail(object data)
         {
             var sqlPath = $"{TableName}/change_email";
@@ -24,6 +46,11 @@ namespace Server.Source.Database
             return DatabaseHelper.GetScalarValue<int>(result);
         }
 
+        /// <summary>
+        /// Thay đổi avatar người dùng.
+        /// </summary>
+        /// <param name="data">Thông tin avatar mới (kiểu ParamsChangeAvatar).</param>
+        /// <returns>1 nếu thành công, 0 nếu thất bại.</returns>
         public virtual int ChangeAvatar(object data)
         {
             var sqlPath = $"{TableName}/change_avatar";
